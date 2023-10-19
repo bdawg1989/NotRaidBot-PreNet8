d9dcb501fc44ac7b522916d79be333dd91f955c7
Gengar <hhbrandon.stone@gmail.com>
2023-10-19 14:33:27 -0500

    Version Change.  4.3

--- SysBot.Pokemon/Helpers/NotRaidBot.cs
+++ SysBot.Pokemon/Helpers/NotRaidBot.cs
@@ -2,6 +2,6 @@
 
 public static class NotRaidBot
 {
-    public const string Version = "v4.2";
+    public const string Version = "v4.3";
     public const string Attribution = "https://notpaldea.net";
 }
