407a576f3f5a6e0ab0e4711b418507b4d2e93e8f
Gengar <hhbrandon.stone@gmail.com>
2023-11-22 13:13:42 -0600

    Version update. 5.8

--- SysBot.Pokemon/Helpers/NotRaidBot.cs
+++ SysBot.Pokemon/Helpers/NotRaidBot.cs
@@ -2,6 +2,6 @@
 
 public static class NotRaidBot
 {
-    public const string Version = "v5.7";
+    public const string Version = "v5.8";
     public const string Attribution = "https://notpaldea.net";
 }
